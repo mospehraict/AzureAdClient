0be9757df58c018b20ba04a2166922242bffee03
Alexander Lazarev <alazarev@microsoft.com>
2015-02-27 16:56:25 -0800

    Basic project and interface

---
+++ AzureAdClient/IActiveDirectoryClient.cs
@@ -0,0 +1,13 @@
+﻿namespace AzureAdClient
+{
+    using System.Security.Claims;
+
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+    interface IActiveDirectoryClient
+    {
+        string AuthenticateCredentials(string tenant, string resource, UserCredential userCredential);
+
+        ClaimsPrincipal AuthenticateToken(string tenant, string resource, string token);
+    }
+}
